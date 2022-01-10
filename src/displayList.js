d7505c693aa4e52caec372c77a14dfb04208a342
jeff <jmorton138@gmail.com>
2022-01-10 17:18:48 +0000

    Deleted unnecessary console logs

--- src/displayList.js
+++ src/displayList.js
@@ -1,11 +1,9 @@
 const displayList = () => {
     const list = document.querySelector('ul');
-    console.log(list)
 
     for (let i = 0; i < localStorage.length; i++) {
         const li = document.createElement('li');
         li.textContent = localStorage.getItem(localStorage.key(i))
-        console.log(li);
         list.appendChild(li);
     }
 
--- src/saveItem.js
+++ src/saveItem.js
@@ -1,6 +1,5 @@
 const saveItem = (input) => {
     const index = localStorage.length + 1
-    console.log(index);
     window.localStorage.setItem(`${index}`,`${input}`);
 }
 
