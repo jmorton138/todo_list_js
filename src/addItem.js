cbee61f0b43de252292f9cd4826b43756d51d55e
jeff <jmorton138@gmail.com>
2022-01-12 14:03:33 +0000

    Deleted add item to display module

--- src/addItem.js
+++
@@ -1,9 +0,0 @@
-import displayItemMin from "./displayItemMin.js";
-
-const addItemToList = (list, index) => {
-    // const list = document.querySelector('.list')
-    //const div = displayItemMin(list, index)
-    // list.appendChild(div);
-}
-
-export default addItemToList;
