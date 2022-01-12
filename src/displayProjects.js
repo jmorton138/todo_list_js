76529777ca30af3bf0ee0ce85fcc8a76d63d00bc
jeff <jmorton138@gmail.com>
2022-01-12 10:30:52 +0000

    Style and layout to add task form

--- src/displayProjects.js
+++ src/displayProjects.js
@@ -3,16 +3,19 @@ import displayList from "./displayList";
 const displayProjects = () => {
     const content = document.getElementById('content');
     var list = document.querySelector('.projects');
+    const heading = document.createElement('h2');
+    heading.textContent = "Projects";
     if (list === null) {
-        list = document.createElement('ul');
+        list = document.createElement('div');
         list.className = "projects";
     } else {
         list.innerHTML = "";
     }
 
+
  
         for (let i = 0; i < localStorage.length; i++) {
-            const li = document.createElement('li');
+            const li = document.createElement('div');
             const name = localStorage.key(i);
             li.textContent = name;
             li.addEventListener('click', () => {
@@ -20,6 +23,7 @@ const displayProjects = () => {
             })
             list.appendChild(li);
         }
+        list.prepend(heading);
         content.appendChild(list);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import HomeDOM from './homeDom.js';
+import './style.css';
 
 window.addEventListener('load', () => {
     HomeDOM();
